1e895a5d81a261a67195ba8397b4927b8b117978
Henry Jones <henadinn+github223@gmail.com>
2026-03-24 11:22:12 +0100

    Initial Commit

---
+++ Places/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Places
+//
+//  Created by henry on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by henry on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
